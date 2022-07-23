7912051b69fbd78cb7a510c2b7462d99ef975f5d
changes <donaldperyyman>
2022-07-23 16:11:07 +0530

    Initial commit

---
+++ app/src/main/java/com/internertspeedmeter/sn/MainActivity.java
@@ -0,0 +1,16 @@
+package com.internertspeedmeter.sn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sn.speedtest.SpeedTestHandler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
